306a258520a2fae084b8998fdfc74f203acf514d
Tace <yalianwork@gmail.com>
2021-02-23 04:58:13 +0800

    add

---
+++ 5.3_say_Hi_for_All_List_Number.py
@@ -0,0 +1,13 @@
+names = ['Allen', 'Tom', 'Mayday', 'JJ', 'Jolin', 'Jay', 'Jam']
+for name in names:
+	print('Hi ', name)
+
+#取字串中字母
+car = 'Audi'
+for c in car:
+	print(c)
+print(len(car))
+print('A' in car)
+print('Au' in car)
+print('Ad' in car)
+print('s' in car)
